00a54ecb803b727da5362f54a85c09c7a77ff1c5
Escalona <tescal2@users.noreply.github.com>
2015-09-29 14:39:36 -0500

    Update router.py

--- router.py
+++ router.py
@@ -3,11 +3,11 @@
 import sys
 
 def getRouter(rtr):
-   router1 = {'os_version':'3.1.1','hostname':'nyc_router1','model':'Nexus 9372PX-E', \
+   router1 = {'os_version':'7.0.1','hostname':'nyc_router1','model':'Nexus 9372PX-E', \
    'domain':'cisco.com','mgmt_ip':'10.10.16.07'}
-   router2 = dict( os_version='3.2.1', hostname='rtp_router2',model='Nexus 9372PX-E', \
+   router2 = dict( os_version='7.0.1', hostname='rtp_router2',model='Nexus 9372PX-E', \
    	domain='cisco.com', mgmt_ip='10.03.19.14')
-   router3 = dict( os_version='3.1.1', hostname='ROUTER3',model='Nexus 9504', \
+   router3 = dict( os_version='7.0.1', hostname='ROUTER3',model='Nexus 9504', \
    	domain='lab.cisco.com', mgmt_ip='10.06.25.90')
    
    router_list = [router1,router2,router3]
